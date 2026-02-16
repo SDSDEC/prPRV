0673c4e3ad6d5a774666f880491e2483a1aa205b
SDSDEC <4elovekluna121206@gmail.com>
2026-02-16 12:31:40 +0700

    6. obrabotka sobitii3 (nerobit click 2 variant i proverochnaya) HEEEELPPP

--- js/main.js
+++ js/main.js
@@ -9,14 +9,18 @@ let app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
         cart: 0,
-
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 })
