a12e685cefd2bf9b44dd101d54dd8677ba87fd4c
SDSDEC <4elovekluna121206@gmail.com>
2026-02-16 12:11:52 +0700

    6. obrabotka sobitii1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-let  app = new Vue({
+let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true,
-        inventory: 2,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green',
+                variantColor: 'green'
             },
             {
                 variantId: 2235,
                 variantColor: 'blue'
             }
         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+        cart: 0,
+
     }
 })
